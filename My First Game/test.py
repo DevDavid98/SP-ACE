f9c112bd3f466908f6215b9a52895eaa13be543f
David Garcia <44665014+DevDavid98@users.noreply.github.com>
2023-03-31 00:07:38 -0500

    Add files via upload
    The necessary files needed for the game. Will be updated as the project grows.

---
+++ My First Game/test.py
@@ -0,0 +1,63 @@
+import pygame
+from sys import exit
+
+def main():
+    pygame.init()
+    set_fps = pygame.time.Clock()
+    screen_name = pygame.display.set_caption('SP-ACE')
+    screen = pygame.display.set_mode((600,600))
+
+    text = pygame.font.Font(None, 50)
+    text_surface = text.render("HELLO WORLD", False, (255,255,255))
+
+    space_img = pygame.image.load('game assets/space_game.jpg').convert_alpha()
+    player_img = pygame.image.load('game assets/player1.png').convert_alpha()
+    player_img = pygame.transform.scale(player_img,(50,50))
+
+    player_rect = player_img.get_rect(center = (300,300))
+
+    enemy_img = pygame.image.load('game assets/enemy1.png').convert_alpha()
+    enemy_img = pygame.transform.scale(enemy_img,(40,40))
+    enemy_rect = enemy_img.get_rect(center = (150,150))
+
+    ship_speed = 5
+    rect_hits = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        set_fps.tick(60)
+        screen.blit(space_img,(0,0))
+        screen.blit(player_img, player_rect)
+        screen.blit(enemy_img, enemy_rect)
+
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[pygame.K_LEFT]:
+            player_rect.centerx -= ship_speed
+        if pressed_key[pygame.K_RIGHT]:
+            player_rect.centerx += ship_speed
+        if pressed_key[pygame.K_UP]:
+            player_rect.centery -= ship_speed
+        if pressed_key[pygame.K_DOWN]:
+            player_rect.centery += ship_speed
+
+        if player_rect.right < 0:
+            player_rect.left = 600
+        elif player_rect.left > 600:
+            player_rect.right = 0
+        
+        if player_rect.bottom < 0:
+            player_rect.top = 600
+        elif player_rect.top > 600:
+            player_rect.bottom = 0
+
+        player_collide = player_rect.colliderect(enemy_rect)
+        if player_collide:
+            rect_hits += 1
+            print(f'PLAYER COLLIED WITH ENEMY {rect_hits} TIMES')
+
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
